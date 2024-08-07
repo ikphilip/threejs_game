cdf4f80d8fcab98c6142898ef217d1c14a7ffc84
Philip Curley <philip@interactiveknowledge.com>
2024-08-07 16:04:12 -0400

    collision counter; touchmove

--- game.js
+++ game.js
@@ -5,11 +5,21 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Game variables.
+const playerRadius = 48
+const bugRadius = 16
+
 // Create a circle.
-const circleGeometry = new THREE.CircleGeometry(48, 64);
+const circleGeometry = new THREE.CircleGeometry(playerRadius, 36);
 const circleMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
 const player = new THREE.Mesh(circleGeometry, circleMaterial);
 scene.add(player);
+
+// Create another circle.
+const circleGeometry2 = new THREE.CircleGeometry(bugRadius, 36);
+const circleMaterial2 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+const bug = new THREE.Mesh(circleGeometry2, circleMaterial2);
+scene.add(bug)
 
 // Add arrow to represent directionality.
 // https://threejs.org/docs/#api/en/helpers/ArrowHelper
@@ -28,17 +38,43 @@ player.add( arrowHelper );
 // Set the camera position
 camera.position.z = 1;
 
+const randomX = () => {
+  return Math.random() * window.innerWidth - window.innerWidth / 2;
+}
+
+const randomY = () => {
+  return Math.random() * window.innerHeight - window.innerHeight / 2;
+}
+
 // Initial game state
 const state = {
   player: { x: 0, y: 0, z: 0 },
   touch: { x: 0, y: 0, z: 0 },
+  bug: { x: randomX(), y: randomY(), z: 0 },
+  counter: 0,
+  currentCollision: false
 };
 
 // Function to update the game state
 const update = elapsed => {
-  // Get the width of the canvas.
   player.position.x = state.player.x;
   player.position.y = state.player.y;
+
+  bug.position.x = state.bug.x;
+  bug.position.y = state.bug.y;
+
+  // Count the number of collisions.
+  if (detectCollision() === true && state.currentCollision === false) {
+    state.currentCollision = true
+    state.counter += 1
+    state.bug.x = randomX()
+    state.bug.y = randomY()
+
+    console.log(state.counter)
+  }
+  else if (detectCollision() === false && state.currentCollision === true) {
+    state.currentCollision = false
+  }
 }
 
 // Function to render the game state
@@ -93,6 +129,20 @@ const rotatePlayerToCoords = (x, y) => {
   player.rotation.z = angle;
 }
 
+// detect collision between player and bug.
+const detectCollision = () => {
+  let dx = state.bug.x - state.player.x;
+  let dy = state.bug.y - state.player.y;
+  let distance = Math.sqrt(dx * dx + dy * dy);
+
+  if (distance < playerRadius) {
+    return true
+  }
+  else {
+    return false
+  }
+}
+
 // Start the game loop
 requestAnimationFrame(gameLoop);
 
@@ -103,4 +153,6 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
+// Add listener for touch events.
 window.addEventListener('touchstart', onTouch);
+window.addEventListener('touchmove', onTouch);
